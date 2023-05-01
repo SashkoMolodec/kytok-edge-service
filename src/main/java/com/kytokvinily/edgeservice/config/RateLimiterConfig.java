42ea23067bb4509aa598114aeb79e62df6a44648
Sashkomolodec <sanykk2014@gmail.com>
2023-05-02 00:28:35 +0300

    security

--- src/main/java/com/kytokvinily/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/com/kytokvinily/edgeservice/config/RateLimiterConfig.java
@@ -5,12 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
 
+import java.security.Principal;
+
 @Configuration
 public class RateLimiterConfig {
 
-	@Bean
-	public KeyResolver keyResolver() {
-		return exchange -> Mono.just("anonymous");
-	}
-	
+    @Bean
+    public KeyResolver keyResolver() {
+        return exchange -> exchange.getPrincipal()
+                .map(Principal::getName)
+                .defaultIfEmpty("anonymous");
+    }
+
 }
